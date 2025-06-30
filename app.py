047d0da8876e6671e7bda22c0741de783c45efec
viejopeter <pquinchanegua@gmail.com>
2025-06-30 15:17:44 +1000

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+# Import libraries
+
+# Instantiate Flask functionality
+
+# Sample data
+
+# Read operation
+
+# Create operation
+
+# Update operation
+
+# Delete operation
+
+# Run the Flask app
+    
